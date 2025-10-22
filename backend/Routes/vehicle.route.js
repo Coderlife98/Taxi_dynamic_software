0f824e072bf718bfa7584eec7ca1170778b5355a
Coderlife98 <anita.hr112@gmail.com>
2025-10-22 12:09:27 +0530

    Data updated successfully while editing

--- backend/Routes/vehicle.route.js
+++ backend/Routes/vehicle.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { addVehicle, deleteVehicleById, editVehicleById, updateVehicle, viewVehicles } from "../Controllers/vehicle/vehicle.min.js";
+import { addVehicle, deleteVehicleById, editVehicleById, viewVehicles } from "../Controllers/vehicle/vehicle.min.js";
 const router = express.Router();
 
 router.post("/addVehicle", addVehicle);
 router.post("/edit/:id", editVehicleById);
 router.post("/view", viewVehicles);
 router.delete("/deleteVehicle/:id", deleteVehicleById);
-router.post("/updateVehicle/:id", updateVehicle);
+// router.post("/updateVehicle/:id", updateVehicle);
 
 
 
