9b36489005d94223ba5c05522cd36962d45db856
Coderlife98 <anita.hr112@gmail.com>
2025-10-22 11:23:17 +0530

    Delete vehicle also completed

--- backend/Routes/vehicle.route.js
+++ backend/Routes/vehicle.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addVehicle, deleteVehicleById, editVehicleById, updateVehicle } from "../Controllers/vehicle/vehicle.min.js";
+import { addVehicle, deleteVehicleById, editVehicleById, updateVehicle, viewVehicles } from "../Controllers/vehicle/vehicle.min.js";
 const router = express.Router();
 
 router.post("/addVehicle", addVehicle);
 router.post("/edit/:id", editVehicleById);
+router.post("/view", viewVehicles);
 router.delete("/deleteVehicle/:id", deleteVehicleById);
 router.post("/updateVehicle/:id", updateVehicle);
 
