69b554553b357c8489554cf6797f6607c6ffbcd6
Coderlife98 <anita.hr112@gmail.com>
2025-10-23 09:41:55 +0530

    Add address is working well

---
+++ backend/Routes/address.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { addAddress, deleteAddress, editAddress, viewAddress } from "../Controllers/address/address.min.js";
+
+const router = express.Router();
+
+router.post("/add", addAddress);
+router.delete("/delete", deleteAddress);
+router.post("/edit/:id", editAddress);
+router.post("/view/:id", viewAddress);
+
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ import { configDotenv } from "dotenv";
 configDotenv();
 const app = express();
 import vehicleRoutes from "./Routes/vehicle.route.js";
+import addressRoutes from "./Routes/address.route.js"
 import { dbConfig } from "./config/dbConfig.js";
 app.use(express.json());
 app.use("/api/", vehicleRoutes);
-
+app.use("/api/address", addressRoutes);
 app.listen(process.env.PORT || 4000, async (req, res) => {
   dbConfig()
   console.log(`server is running at port ${process.env.PORT} `)
