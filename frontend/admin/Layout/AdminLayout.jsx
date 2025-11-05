3a552eda6862ca54bbcb4bfe38fc844bdaca2bda
Coderlife98 <anita.hr112@gmail.com>
2025-11-05 17:59:54 +0530

    working on booking part

--- frontend/admin/Layout/AdminLayout.jsx
+++ frontend/admin/Layout/AdminLayout.jsx
@@ -7,7 +7,7 @@ const AdminLayout = () => {
   return (
     <div>
       <Header />
-      <div className="flex bg-[url('./../src/assets/Admin/bg_admin.jpg')] bg-cover bg-center h-screen">
+      <div className="flex bg-[url('./../src/assets/Admin/bg_admin.jpg')] bg-cover bg-center min-h-screen">
         <div className='lg:w-[230px] z-30 top-10 scrollbar-hidden max-h-screen overflow-y-scroll fixed  border-r-2 border-white'>
           <LeftBar />
         </div>
--- frontend/admin/pages/Booking.jsx
+++ frontend/admin/pages/Booking.jsx
@@ -4,40 +4,39 @@ const Booking = () => {
   return (
     <div className='flex w-full h-screen justify-center items-center'>
       <form
-        className="border border-slate-500 p-5"
+        className="border border-slate-500 max-w-10/12 w-full p-5"
       >
         <h2 className="text-lg text-white font-bold mb-4">Add Booking</h2>
 
-        <div className="grid md:grid-cols-2 gap-4">
+        <div className="grid md:grid-cols-2 text-white  gap-4">
           {/* Left column */}
           <div>
-            <input
-              type="text"
-              placeholder="Address"
-              name='address'
-              className="border border-slate-300 text-white w-full my-2 py-1 px-3"
-            />
-            <input
-              type="text"
-              placeholder="Pincode"
-              name='pincode'
-              className="border border-slate-300 text-white w-full my-2 py-1 px-3"
-            />
+            <label htmlFor="from">From</label>
+            <select name="" id="from" className='border border-slate-300 text-white w-full py-1 px-3'>
+              <option value=""></option>
+            </select>
           </div>
-
-          {/* Status */}
           <div>
-            <select
-              name='status'
-              className="border border-slate-300 text-white w-full py-1 px-3"
-            >
-              <option class="text-black" value="">Select Status</option>
-              <option class="text-black" value="True">True</option>
-              <option class="text-black" value="False">False</option>
+            <label htmlFor="to">To</label>
+            <select name="" id="to" className='border border-slate-300 text-white w-full py-1 px-3'>
+              <option value=""></option>
             </select>
           </div>
         </div>
 
+        <div className="grid md:grid-cols-2 text-white  my-3 gap-4">
+          {/* Left column */}
+          <div>
+            <label htmlFor="vehicles">Vehicles</label>
+            <select name="" id="vehicles" className='border border-slate-300 text-white w-full py-1 px-3'>
+              <option value=""></option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="price">Price</label>
+            <input type="text" name='price' id='price' className='border border-slate-300 text-white w-full py-1 px-3' />
+          </div>
+        </div>
         <button
           type="submit"
           className="py-2 mt-5 w-full rounded-b-md bg-green-700 text-white"
